c1a7481174e17d76b2aefcb17204857a21246076
李汪翰 <lee.wanghan@gmail.com>
2024-01-21 13:56:37 +0800

    Uploading prototype

---
+++ prototype/main.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+class RacketPrototype(ABC):
+    def __init__(self, brand, model, weight, balancePoint):
+        self.brand = brand
+        self.model = model
+        self.weight = weight
+        self.balancePoint = balancePoint
+
+    @abstractmethod
+    def clone(self):
+        pass
+
+class WilsonTennisRacket(RacketPrototype):
+    def __init__(self, brand, model, weight, balancePoint):
+        super().__init__(brand, model, weight, balancePoint)
+
+    def clone(self):
+        print("Wilson clone: {} {} {} {}".format(self.brand, self.model, self.weight, self.balancePoint))
+        return WilsonTennisRacket(self.brand, self.model, self.weight, self.balancePoint)
+    
+class HeadTennisRacket(RacketPrototype):
+    def __init__(self, brand, model, weight, balancePoint):
+        super().__init__(brand, model, weight, balancePoint)
+
+    def clone(self):
+        print("Wilson clone: {} {} {} {}".format(self.brand, self.model, self.weight, self.balancePoint))
+        return HeadTennisRacket(self.brand, self.model, self.weight, self.balancePoint)
+    
+class TennisRacketPrototypeManager:
+    racketMap = {}
+
+    @staticmethod
+    def getCloneRacket(model):
+        r = TennisRacketPrototypeManager.racketMap.get(model)
+        return r.clone()
+    
+    @staticmethod
+    def buildPrototypes():
+        r1 = WilsonTennisRacket("Wilson", "Pro Staff", 320.0, 32.5)
+        TennisRacketPrototypeManager.racketMap["pro staff"] = r1
+        h1 = HeadTennisRacket("Head", "Graphene 360 Speed", 320.0, 32.5)
+        TennisRacketPrototypeManager.racketMap["graphene 360"] = h1
+
+def main():
+    TennisRacketPrototypeManager.buildPrototypes()
+    for i in range(10):
+        TennisRacketPrototypeManager.getCloneRacket("pro staff")
+
+if __name__ == "__main__":
+    main()
